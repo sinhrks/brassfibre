977d4b243a5467b5b28fa7b87d3eb99668e0d561
sinhrks <sinhrks@gmail.com>
2016-02-20 22:13:31 +0900

    ENH: Add reference ops

--- src/block/ops.rs
+++ src/block/ops.rs
@@ -6,7 +6,31 @@ use std::ops::{Add, Mul, Sub, Div, Rem};
 
 use super::Block;
 
-macro_rules! define_numric_op(
+
+fn broadcast<T>(left: &Vec<Vec<T>>, func: &Fn(&T) -> T) -> Vec<Vec<T>> {
+    let mut new_values: Vec<Vec<T>> = vec![];
+    for value in left.iter() {
+        let new_value = value.iter().map(func).collect();
+        new_values.push(new_value);
+    }
+    return new_values;
+}
+
+fn elemwise<T>(left: &Vec<Vec<T>>, right: &Vec<Vec<T>>,
+            func: &Fn((&T, &T)) -> T) -> Vec<Vec<T>>
+    where T: Copy + Num {
+
+    let mut new_values: Vec<Vec<T>> = vec![];
+    for (value, rvalue) in left.iter().zip(right.iter()) {
+        let new_value = value.iter()
+                             .zip(rvalue.iter())
+                             .map(func).collect();
+        new_values.push(new_value);
+    }
+    return new_values;
+}
+
+macro_rules! define_numeric_op(
   ($t:ident $m:ident) => (
 
     // Broadcast
@@ -17,11 +41,49 @@ macro_rules! define_numric_op(
 
         type Output = Block<T, U, V>;
         fn $m(self, _rhs: T) -> Block<T, U, V> {
-            let mut new_values: Vec<Vec<T>> = vec![];
-            for value in self.values {
-                let new_value = value.iter().map(|x: &T| (*x).$m(_rhs)).collect();
-                new_values.push(new_value);
-            }
+            let new_values = broadcast(&self.values, &|x| (*x).$m(_rhs));
+            return Block::from_nested_vec(new_values,
+                                          self.index.copy_values(),
+                                          self.columns.copy_values());
+        }
+    }
+
+    impl<'a, T, U, V> $t<&'a T> for Block<T, U, V>
+        where T: Copy + Num,
+              U: Copy + Eq + Hash,
+              V: Copy + Eq + Hash {
+
+        type Output = Block<T, U, V>;
+        fn $m(self, _rhs: &T) -> Block<T, U, V> {
+            let new_values = broadcast(&self.values, &|x: &T| (*x).$m(*_rhs));
+            return Block::from_nested_vec(new_values,
+                                          self.index.copy_values(),
+                                          self.columns.copy_values());
+        }
+    }
+
+    impl<'b, T, U, V> $t<T> for &'b Block<T, U, V>
+        where T: Copy + Num,
+              U: Copy + Eq + Hash,
+              V: Copy + Eq + Hash {
+
+        type Output = Block<T, U, V>;
+        fn $m(self, _rhs: T) -> Block<T, U, V> {
+            let new_values = broadcast(&self.values, &|x: &T| (*x).$m(_rhs));
+            return Block::from_nested_vec(new_values,
+                                          self.index.copy_values(),
+                                          self.columns.copy_values());
+        }
+    }
+
+    impl<'a, 'b, T, U, V> $t<&'a T> for &'b Block<T, U, V>
+        where T: Copy + Num,
+              U: Copy + Eq + Hash,
+              V: Copy + Eq + Hash {
+
+        type Output = Block<T, U, V>;
+        fn $m(self, _rhs: &T) -> Block<T, U, V> {
+            let new_values = broadcast(&self.values, &|x: &T| (*x).$m(*_rhs));
             return Block::from_nested_vec(new_values,
                                           self.index.copy_values(),
                                           self.columns.copy_values());
@@ -32,23 +94,61 @@ macro_rules! define_numric_op(
     impl<T, U, V> $t<Block<T, U, V>> for Block<T, U, V>
         where T: Copy + Num,
               U: Copy + Eq + Hash,
-            V: Copy + Eq + Hash {
+              V: Copy + Eq + Hash {
 
         type Output = Block<T, U, V>;
         fn $m(self, _rhs: Block<T, U, V>) -> Block<T, U, V> {
-            if !self.index.equals(&_rhs.index) {
-                panic!("index must be the same!");
-            }
-            if !self.columns.equals(&_rhs.columns) {
-                panic!("columns must be the same!");
-            }
-            let mut new_values: Vec<Vec<T>> = vec![];
-            for (value, rvalue) in self.values.iter().zip(_rhs.values.iter()) {
-                let new_value = value.iter()
-                                     .zip(rvalue.iter())
-                                     .map(|(x, y)| (*x).$m(*y)).collect();
-                new_values.push(new_value);
-            }
+            self.assert_binop(&_rhs);
+            let new_values = elemwise(&self.values, &_rhs.values,
+                                      &|(x, y)| (*x).$m(*y));
+            return Block::from_nested_vec(new_values,
+                                          self.index.copy_values(),
+                                          self.columns.copy_values());
+        }
+    }
+
+    impl<'a, T, U, V> $t<&'a Block<T, U, V>> for Block<T, U, V>
+        where T: Copy + Num,
+              U: Copy + Eq + Hash,
+              V: Copy + Eq + Hash {
+
+        type Output = Block<T, U, V>;
+        fn $m(self, _rhs: &Block<T, U, V>) -> Block<T, U, V> {
+            self.assert_binop(&_rhs);
+            let new_values = elemwise(&self.values, &_rhs.values,
+                                      &|(x, y)| (*x).$m(*y));
+            return Block::from_nested_vec(new_values,
+                                          self.index.copy_values(),
+                                          self.columns.copy_values());
+        }
+    }
+
+    impl<'b, T, U, V> $t<Block<T, U, V>> for &'b Block<T, U, V>
+        where T: Copy + Num,
+              U: Copy + Eq + Hash,
+              V: Copy + Eq + Hash {
+
+        type Output = Block<T, U, V>;
+        fn $m(self, _rhs: Block<T, U, V>) -> Block<T, U, V> {
+            self.assert_binop(&_rhs);
+            let new_values = elemwise(&self.values, &_rhs.values,
+                                      &|(x, y)| (*x).$m(*y));
+            return Block::from_nested_vec(new_values,
+                                          self.index.copy_values(),
+                                          self.columns.copy_values());
+        }
+    }
+
+    impl<'a, 'b, T, U, V> $t<&'a Block<T, U, V>> for &'b Block<T, U, V>
+        where T: Copy + Num,
+              U: Copy + Eq + Hash,
+              V: Copy + Eq + Hash {
+
+        type Output = Block<T, U, V>;
+        fn $m(self, _rhs: &Block<T, U, V>) -> Block<T, U, V> {
+            self.assert_binop(&_rhs);
+            let new_values = elemwise(&self.values, &_rhs.values,
+                                      &|(x, y)| (*x).$m(*y));
             return Block::from_nested_vec(new_values,
                                           self.index.copy_values(),
                                           self.columns.copy_values());
@@ -58,11 +158,12 @@ macro_rules! define_numric_op(
   );
 );
 
-define_numric_op!(Add add);
-define_numric_op!(Mul mul);
-define_numric_op!(Sub sub);
-define_numric_op!(Div div);
-define_numric_op!(Rem rem);
+define_numeric_op!(Add add);
+define_numeric_op!(Mul mul);
+define_numeric_op!(Sub sub);
+define_numeric_op!(Div div);
+define_numeric_op!(Rem rem);
+
 
 #[cfg(test)]
 mod tests {
@@ -114,6 +215,29 @@ mod tests {
     }
 
     #[test]
+    fn test_block_ops_i64_broadcast_refs() {
+        let b = Block::from_col_vec(vec![1, 2, 3, 4, 5, 6],
+                                    vec![10, 20, 30], vec!["X", "Y"]);
+        let mut result = &b + 3;
+        assert_eq!(&result.get_column_by_label(&"X").values, &vec![4, 5, 6]);
+        assert_eq!(&result.get_column_by_label(&"Y").values, &vec![7, 8, 9]);
+        assert_eq!(&result.index.values, &vec![10, 20, 30]);
+        assert_eq!(&result.columns.values, &vec!["X", "Y"]);
+
+        let mut result = &b + &3;
+        assert_eq!(&result.get_column_by_label(&"X").values, &vec![4, 5, 6]);
+        assert_eq!(&result.get_column_by_label(&"Y").values, &vec![7, 8, 9]);
+        assert_eq!(&result.index.values, &vec![10, 20, 30]);
+        assert_eq!(&result.columns.values, &vec!["X", "Y"]);
+
+        let mut result = b + &3;
+        assert_eq!(&result.get_column_by_label(&"X").values, &vec![4, 5, 6]);
+        assert_eq!(&result.get_column_by_label(&"Y").values, &vec![7, 8, 9]);
+        assert_eq!(&result.index.values, &vec![10, 20, 30]);
+        assert_eq!(&result.columns.values, &vec!["X", "Y"]);
+    }
+
+    #[test]
     fn test_block_ops_f64_broadcast() {
         let b = Block::from_col_vec(vec![1., 2., 3., 4., 5., 6.],
                                     vec![10, 20, 30], vec!["X", "Y"]);
@@ -153,6 +277,29 @@ mod tests {
         let mut result = b % 2.;
         assert_eq!(&result.get_column_by_label(&"X").values, &vec![1., 0., 1.]);
         assert_eq!(&result.get_column_by_label(&"Y").values, &vec![0., 1., 0.]);
+        assert_eq!(&result.index.values, &vec![10, 20, 30]);
+        assert_eq!(&result.columns.values, &vec!["X", "Y"]);
+    }
+
+    #[test]
+    fn test_block_ops_f64_broadcast_refs() {
+        let b = Block::from_col_vec(vec![1., 2., 3., 4., 5., 6.],
+                                    vec![10, 20, 30], vec!["X", "Y"]);
+        let mut result = &b + 3.;
+        assert_eq!(&result.get_column_by_label(&"X").values, &vec![4., 5., 6.]);
+        assert_eq!(&result.get_column_by_label(&"Y").values, &vec![7., 8., 9.]);
+        assert_eq!(&result.index.values, &vec![10, 20, 30]);
+        assert_eq!(&result.columns.values, &vec!["X", "Y"]);
+
+        let mut result = &b + &3.;
+        assert_eq!(&result.get_column_by_label(&"X").values, &vec![4., 5., 6.]);
+        assert_eq!(&result.get_column_by_label(&"Y").values, &vec![7., 8., 9.]);
+        assert_eq!(&result.index.values, &vec![10, 20, 30]);
+        assert_eq!(&result.columns.values, &vec!["X", "Y"]);
+
+        let mut result = b + &3.;
+        assert_eq!(&result.get_column_by_label(&"X").values, &vec![4., 5., 6.]);
+        assert_eq!(&result.get_column_by_label(&"Y").values, &vec![7., 8., 9.]);
         assert_eq!(&result.index.values, &vec![10, 20, 30]);
         assert_eq!(&result.columns.values, &vec!["X", "Y"]);
     }
@@ -212,6 +359,33 @@ mod tests {
     }
 
     #[test]
+    fn test_block_ops_i64_elemwise_refs() {
+        let b = Block::from_col_vec(vec![1, 2, 3, 4, 5, 6],
+                                    vec![10, 20, 30], vec!["X", "Y"]);
+        let r = Block::from_col_vec(vec![2, 3, 1, 2, 3, 1],
+                                    vec![10, 20, 30], vec!["X", "Y"]);
+        let mut result = &b + r;
+        assert_eq!(&result.get_column_by_label(&"X").values, &vec![3, 5, 4]);
+        assert_eq!(&result.get_column_by_label(&"Y").values, &vec![6, 8, 7]);
+        assert_eq!(&result.index.values, &vec![10, 20, 30]);
+        assert_eq!(&result.columns.values, &vec!["X", "Y"]);
+
+        let r = Block::from_col_vec(vec![2, 3, 1, 2, 3, 1],
+                                    vec![10, 20, 30], vec!["X", "Y"]);
+        let mut result = &b + &r;
+        assert_eq!(&result.get_column_by_label(&"X").values, &vec![3, 5, 4]);
+        assert_eq!(&result.get_column_by_label(&"Y").values, &vec![6, 8, 7]);
+        assert_eq!(&result.index.values, &vec![10, 20, 30]);
+        assert_eq!(&result.columns.values, &vec!["X", "Y"]);
+
+        let mut result = b + &r;
+        assert_eq!(&result.get_column_by_label(&"X").values, &vec![3, 5, 4]);
+        assert_eq!(&result.get_column_by_label(&"Y").values, &vec![6, 8, 7]);
+        assert_eq!(&result.index.values, &vec![10, 20, 30]);
+        assert_eq!(&result.columns.values, &vec!["X", "Y"]);
+    }
+
+    #[test]
     fn test_block_ops_f64_elemwise() {
         let b = Block::from_col_vec(vec![1., 2., 3., 4., 5., 6.],
                                     vec![10, 20, 30], vec!["X", "Y"]);
@@ -261,6 +435,34 @@ mod tests {
         let mut result = b % r;
         assert_eq!(&result.get_column_by_label(&"X").values, &vec![1., 2., 0.]);
         assert_eq!(&result.get_column_by_label(&"Y").values, &vec![0., 2., 0.]);
+        assert_eq!(&result.index.values, &vec![10, 20, 30]);
+        assert_eq!(&result.columns.values, &vec!["X", "Y"]);
+    }
+
+    #[test]
+    fn test_block_ops_f64_elemwise_refs() {
+        let b = Block::from_col_vec(vec![1., 2., 3., 4., 5., 6.],
+                                    vec![10, 20, 30], vec!["X", "Y"]);
+        let r = Block::from_col_vec(vec![2., 3., 1., 2., 3., 1.],
+                                    vec![10, 20, 30], vec!["X", "Y"]);
+
+        let mut result = &b + r;
+        assert_eq!(&result.get_column_by_label(&"X").values, &vec![3., 5., 4.]);
+        assert_eq!(&result.get_column_by_label(&"Y").values, &vec![6., 8., 7.]);
+        assert_eq!(&result.index.values, &vec![10, 20, 30]);
+        assert_eq!(&result.columns.values, &vec!["X", "Y"]);
+
+        let r = Block::from_col_vec(vec![2., 3., 1., 2., 3., 1.],
+                                    vec![10, 20, 30], vec!["X", "Y"]);
+        let mut result = &b + &r;
+        assert_eq!(&result.get_column_by_label(&"X").values, &vec![3., 5., 4.]);
+        assert_eq!(&result.get_column_by_label(&"Y").values, &vec![6., 8., 7.]);
+        assert_eq!(&result.index.values, &vec![10, 20, 30]);
+        assert_eq!(&result.columns.values, &vec!["X", "Y"]);
+
+        let mut result = b + &r;
+        assert_eq!(&result.get_column_by_label(&"X").values, &vec![3., 5., 4.]);
+        assert_eq!(&result.get_column_by_label(&"Y").values, &vec![6., 8., 7.]);
         assert_eq!(&result.index.values, &vec![10, 20, 30]);
         assert_eq!(&result.columns.values, &vec!["X", "Y"]);
     }
